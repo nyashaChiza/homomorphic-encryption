27d7c883ee3dc5c52a21b3336cb03a39ffd338d3
Nyasha Chizampeni <nchizampeni@gmail.com>
2024-11-25 08:17:20 +0200

    wip

--- start-project.bat
+++ start-project.bat
@@ -10,5 +10,7 @@ REM Change the path to your Django project
 set DJANGO_PROJECT_PATH=C:\Users\User\Documents\projects\homomorphic-encryption
 
 REM Start the Django development server
+
 cd %DJANGO_PROJECT_PATH%
+python manage.py migrate
 python manage.py runserver
